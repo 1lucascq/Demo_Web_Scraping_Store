6d61c8ee278718e694590e0986144cf34bb7eb94
Lucas Quadros <1lucascq@gmail.com>
2023-04-11 22:54:20 -0300

    setup: update names, imports and add mocked data

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://live-lexart-corp-site.pantheonsite.io/wp-content/uploads/2022/12/lex-white.svg" alt="company logo" />
+            <p>this is a header</p>
+        </header>
+    )
+}
+
+export default Header
