bae1508b3df0bc34f8a22533f7f8285f2db174ab
lucas.quadros <1lucascq@gmail.com.br>
2024-09-10 19:56:07 -0300

    refactor: Remove unused files and update dependencies
    - Remove unused CSS and JavaScript files
    - Update "next" dependency to version 14.2.9

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
 // pages/index.jsx
 import React from 'react';
 import Filters from '../components/Filters';
-import Header from '@/components/Header';
-import Cards from '@/components/Cards';
-import Footer from '@/components/Footer';
+import Header from '../components/Header';
+import Cards from '../components/Cards';
+import Footer from '../components/Footer';
 
 const Home = ({ scrapedData }) => {
 	console.log('a', scrapedData[0].name);
@@ -21,7 +21,7 @@ const Home = ({ scrapedData }) => {
 };
 
 export async function getServerSideProps() {
-    const URL = 'http://localhost:3000'
+    const URL = process.env.URL || 'http://localhost:3000';
 	const res = await fetch(`${URL}/api/scrape`);
 	const data = await res.json();
     console.log(data.data[0].name)
