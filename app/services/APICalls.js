7971daf8b47df6ab5894b95e74403f6671535a3d
lucas.quadros <1lucascq@gmail.com.br>
2024-09-12 00:08:54 -0300

    refactor: Standardize and optimize API data fetching and processing

--- app/services/APICalls.js
+++ app/services/APICalls.js
@@ -1,32 +1,51 @@
 const ML_LINKS = {
-    geladeira: 'https://api.mercadolibre.com//sites/MLB/search?category=MLB181294',
-    tv: 'https://api.mercadolibre.com//sites/MLB/search?category=MLB1002',
-    celular: 'https://api.mercadolibre.com//sites/MLB/search?category=MLB1055',
-}
+	geladeira: 'https://api.mercadolibre.com//sites/MLB/search?category=MLB181294',
+	tv: 'https://api.mercadolibre.com//sites/MLB/search?category=MLB1002',
+	celular: 'https://api.mercadolibre.com//sites/MLB/search?category=MLB1055',
+};
 
 const BUSCAPE_LINKS = {
-    geladeira: 'https://demo-web-scraping-back.vercel.app/geladeira',
-    tv: 'https://demo-web-scraping-back.vercel.app/tv',
-    celular: 'https://demo-web-scraping-back.vercel.app/celular',
-}
+	geladeira: 'https://demo-web-scraping-back.vercel.app/geladeira',
+	tv: 'https://demo-web-scraping-back.vercel.app/tv',
+	celular: 'https://demo-web-scraping-back.vercel.app/celular',
+};
+
+const standardizeMLData = (data) => {
+	return data.map(product => {
+		const { title, price, thumbnail, permalink, id } = product;
+		return { title, price, thumbnail, store: 'Mercado Livre', id, link: permalink };
+	});
+};
 
 const fetchData = async (url) => {
-    const response = await fetch(url);
-    const fridgesData = await response.json();
+	const response = await fetch(url);
+	const fridgesData = await response.json();
 
-    return fridgesData;
-}
+	return fridgesData;
+};
 
 const fetchMlData = {
-    geladeira: async () => (await fetchData(ML_LINKS.geladeira)).results,
-    tv: async () => (await fetchData(ML_LINKS.tv)).results,
-    celular: async () => (await fetchData(ML_LINKS.celular)).results
-}
+	geladeira: async () => {
+		const data = await fetchData(ML_LINKS.geladeira);
+		const standardizedData = standardizeMLData(data.results);
+		return standardizedData;
+	},
+	tv: async () => {
+		const data = await fetchData(ML_LINKS.tv);
+		const standardizedData = standardizeMLData(data.results);
+		return standardizedData;
+	},
+	celular: async () => {
+		const data = await fetchData(ML_LINKS.celular);
+		const standardizedData = standardizeMLData(data.results);
+		return standardizedData;
+	},
+};
 
 const fetchBuscapeData = {
-    geladeira: async () => await fetchData(BUSCAPE_LINKS.geladeira, {mode: 'no-cors'}),
-    tv: async () => await fetchData(BUSCAPE_LINKS.tv, {mode: 'no-cors'}),
-    celular: async () => await fetchData(BUSCAPE_LINKS.celular, {mode: 'no-cors'})
-}
+	geladeira: async () => await fetchData(BUSCAPE_LINKS.geladeira, { mode: 'no-cors' }),
+	tv: async () => await fetchData(BUSCAPE_LINKS.tv, { mode: 'no-cors' }),
+	celular: async () => await fetchData(BUSCAPE_LINKS.celular, { mode: 'no-cors' }),
+};
 
-export {fetchMlData, fetchBuscapeData};
+export { fetchMlData };
